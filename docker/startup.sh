ca734a6babc494a08902a17bfd433b0e95e93226
Ellie <me@silkky.dev>
2022-08-04 19:52:09 +1000

    Make startup.sh script POSIX compliant

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 patch_searxng_settings() {
   CONF="$1"
@@ -6,31 +6,31 @@ patch_searxng_settings() {
   sed -i -e "s|SECRET_KEY|$(openssl rand -hex 32)|g" "${CONF}"
 
   # Server Settings
-  if [[ -v BASE_URL ]]; then
+  if [ -n "${BASE_URL}" ]; then
     sed -i -e "s|base_url: false|base_url: \"${BASE_URL}\"|g" "${CONF}"
   fi
-  if [[ -v LIMITER ]] && [[ "${LIMITER}" == "true" ]]; then
+  if [ -n "${LIMITER}" ] && [ "${LIMITER}" = "true" ]; then
     sed -i -e "s|limiter: false|limiter: true|g" "${CONF}"
   fi
-  if [[ -v IMAGE_PROXY ]] && [[ "${IMAGE_PROXY}" == "true" ]]; then
+  if [ -n "${IMAGE_PROXY}" ] && [ "${IMAGE_PROXY}" = "true" ]; then
     sed -i -e "s|image_proxy: false|image_proxy: true|g" "${CONF}"
   fi
 
   # General Settings
-  if [[ -v DEBUG ]] && [[ "${DEBUG}" == "true" ]]; then
+  if [ -n "${DEBUG}" ] && [ "${DEBUG}" = "true" ]; then
     sed -i -e "s|debug: false|debug: true|g" "${CONF}"
   fi
-  if [[ -v INSTANCE_NAME ]]; then
+  if [ -n "${INSTANCE_NAME}" ]; then
     sed -i -e "s|instance_name: \"SearXNG\"|instance_name: \"${INSTANCE_NAME}\"|g" "${CONF}"
   fi
-  if [[ -v PRIVACY_POLICY_URL ]]; then
+  if [ -n "${PRIVACY_POLICY_URL}" ]; then
     sed -i -e "s|privacypolicy_url: false|privacypolicy_url: \"${PRIVACY_POLICY_URL}\"|g" "${CONF}"
   fi
-  if [[ -v DONATION_URL ]]; then
+  if [ -n "${DONATION_URL}" ]; then
     sed -i -e "s|donation_url: false|donation_url: \"${DONATION_URL}\"|g" "${CONF}"
   fi
 
-  if [[ -v CUSTOM_BRAND ]] && [[ "${CUSTOM_BRAND}" == "true" ]]; then
+  if [ -n "${CUSTOM_BRAND}" ] && [ "${CUSTOM_BRAND}" = "true" ]; then
     DEFAULT_ISSUE_URL="https://github.com/searxng/searxng/issues"
     ISSUE_URL="${ISSUE_URL:-${DEFAULT_ISSUE_URL}}"
     DEFAULT_DOCS_URL="https://docs.searxng.org"
@@ -51,7 +51,7 @@ EOF
   fi
 
   # Redis Settings
-  if [[ -v REDIS_URL ]]; then
+  if [ -n "${REDIS_URL}" ]; then
     cat >> "${CONF}" <<EOF
 
 redis:
@@ -60,7 +60,7 @@ EOF
   fi
 
   # Outgoing Settings
-  if [[ -v CUSTOM_OUTGOING ]] && [[ "${CUSTOM_OUTGOING}" == "true" ]]; then
+  if [ -n "${CUSTOM_OUTGOING}" ] && [ "${CUSTOM_OUTGOING}" = "true" ]; then
     DEFAULT_REQUEST_TIMEOUT="3.0"
     REQUEST_TIMEOUT="${REQUEST_TIMEOUT:-${DEFAULT_REQUEST_TIMEOUT}}"
     DEFAULT_USERAGENT_SUFFIX=""
@@ -82,7 +82,7 @@ outgoing:
   enable_http2: ${ENABLE_HTTP2}
 EOF
 
-    if [[ -v OUTGOING_PROXIES ]]; then
+    if [ -n "${OUTGOING_PROXIES}" ]; then
       cat >> "${CONF}" << EOF
   proxies:
     all://:
@@ -105,7 +105,7 @@ export BIND_ADDRESS="${BIND_ADDRESS:-${DEFAULT_BIND_ADDRESS}}"
 
 patch_searxng_settings "${SEARXNG_SETTINGS_PATH}"
 
-SEARXNG_VERSION="$(<VERSION)"
+SEARXNG_VERSION="$(cat VERSION)"
 export SEARXNG_VERSION
 echo "SearXNG version ${SEARXNG_VERSION}"
 
